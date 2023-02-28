99a0dd88fff2ba9c470a6ed2552c515690d5cd63
mYcroSoft <110401497+mYYcroSoft@users.noreply.github.com>
2023-02-28 08:25:22 +0100

    text ord

---
+++ index.py
@@ -0,0 +1,17 @@
+text = "Hello world"
+text_space = text.split(' ')
+text_word = list(text)
+print(text_space)
+print(text_word)
+
+
+def text_int_generate():
+    text_int = []
+    for c in text_word:
+        c = ord(c)
+        text_int.append(c)
+    else:
+      
+        return text_int
+
+text_int = text_int_generate()
