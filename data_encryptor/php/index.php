6753a91957fd5f2b4fe5b7c6c1326d16a190618d
mYcroSoft <110401497+mYYcroSoft@users.noreply.github.com>
2023-05-17 21:54:57 +0200

    TEXT

--- data_encryptor/php/index.php
+++ data_encryptor/php/index.php
@@ -5,13 +5,13 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
-    <title>LUA | Encryptor</title>
+    <title>Python ENCRYPTOR</title>
     <link rel="stylesheet" href="css/css.css">
 </head>
 <body>
 
     <div class="left-bar">
-        <div class="bar-title">LUA Encryptor</div>
+        <div class="bar-title">PY ENCRYPTOR</div>
         <br>
         <div class="bar-item" onclick="php_func()">Secure check</div>
         <div class="bar-item">Load files</div>
