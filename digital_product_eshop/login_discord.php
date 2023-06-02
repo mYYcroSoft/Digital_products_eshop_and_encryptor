8770f65c7529f0bb90b611b57d3dfa005d22501f
mYcroSoft <110401497+mYYcroSoft@users.noreply.github.com>
2023-06-02 12:09:19 +0200

    html, user ui

--- digital_product_eshop/login_discord.php
+++ digital_product_eshop/login_discord.php
@@ -1,11 +1,32 @@
 <?php 
+$user_data = array('user_name' => 'demo', 'user_id' => '0123456789');
 
-function redirect_to_discord(){
+beta_login();
+function beta_login(){
 
-    
+
+
+
 }
 
+insrt_new_user();
 
+function insrt_new_user(){
+    $servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "digital_eshop";
+global $user_data;
 
-
+    // Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection 
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+$time = date("H:i", strtotime("now"));
+$sql = "INSERT INTO users (discord_id, first_login_date, last_login_time)
+VALUES ({$user_data['user_id']}, {$time}, {$time})";
+echo $conn->query($sql);
+}
 ?> 
--- digital_product_eshop/user_order_view/aseets/index.js
+++ digital_product_eshop/user_order_view/aseets/index.js
@@ -1,5 +1,26 @@
 
+/*$('#pages_container').load("pages/home_page/user_home.html");*/
+function open_home_page(){
+    $('#pages_container').load("pages/home_page/user_home.html");
+    document.getElementById('pages_container').animate([{ transform: "translateY(-100%)" },{ transform: "translateX(auto)" },],{ duration: 400, });
+}
 
-function open_page(){
-    $('#pages_container').load("pages/page_.html");
+
+function open_orders_page(){
+    $('#pages_container').load("pages/contracts_page/orders_page.html");
+    document.getElementById('pages_container').animate([{ transform: "translateY(-100%)" },{ transform: "translateX(auto)" },],{ duration: 400, });
+
+}
+
+$('#pages_container').load(`pages/orders/orders.html`);
+
+let time = 400
+function open_page(page){
+    document.getElementById('pages_container').animate([{ transform: "translateY(0%)" },{ transform: "translateY(-190%)" },],{ duration: time + 50, });
+    setTimeout(() => { 
+    $('#pages_container').load(`pages/${page}/${page}.html`);
+    document.getElementById('pages_container').animate([{ transform: "translateY(-100%)" },{ transform: "translateX(auto)" },],{ duration: time, });
+    }, time)
+    
+    
 }
